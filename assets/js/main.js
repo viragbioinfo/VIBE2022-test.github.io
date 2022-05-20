ea78925442c91fc17edc9e6b46a4de1de853b364
VIBE Conference <105758425+VIBE2022@users.noreply.github.com>
2022-05-20 02:44:41 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,10 @@
     const map = window.conference.map;
 
     if (typeof map !== 'undefined') {
-        let main_station = L.marker([47.37785, 8.54035], {
+        let main_station = L.marker([52.67846, -8.56815], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fas fa-train"></span> Main Station',
+                html: '<span class="fas fa-train"></span> University',
                 iconSize: [120, 56]
             })
         }).addTo(map);
