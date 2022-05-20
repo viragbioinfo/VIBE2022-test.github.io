2ff84a07ba19ffa39c6856577bb98024c4a3595e
VIBE Conference <105758425+VIBE2022@users.noreply.github.com>
2022-05-20 02:42:49 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
 ---
 ---
 
+
 {% include js/conference.js %}
+
+(() => {
+    const map = window.conference.map;
+
+    if (typeof map !== 'undefined') {
+        let main_station = L.marker([47.37785, 8.54035], {
+            icon: L.divIcon({
+                className: '',
+                html: '<span class="fas fa-train"></span> Main Station',
+                iconSize: [120, 56]
+            })
+        }).addTo(map);
+    }
+})();
+
+
